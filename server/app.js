19197f021da3d59f6a40fe766a84a2fac9550cd3
balogun <balogunbiola101@gmail.com>
2018-11-05 14:38:55 +0100

    setup babel

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/api/v1', (req, res) => {
+  res.status(200).send({ message: 'welcome to SendIT' });
+});
+
+
+
+app.use('*', (req, res) => {
+  res.status(404).json({ message:  'Page Not Found. Please go to /api/v1/parcelOrders to use our api' });
+}); 
+
+app.listen( process.env.PORT || 5000, () => console.log( 'Server started at localhost:5000'));
+
