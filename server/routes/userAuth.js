2080e48ffed45ad0e7946d77bb2e95c8f0bda8e6
balogun <balogunbiola101@gmail.com>
2018-11-21 11:24:25 +0100

    [feature #162113560] User-Sign-In-Endpoint

--- server/routes/userAuth.js
+++ server/routes/userAuth.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
-import userAuthControllerClass from '../controllers/userController';
+import userAuthControllerClass from '../controllers/userAuthController';
 
-const { createUser } = userAuthControllerClass;
+const { createUser, login } = userAuthControllerClass;
 
 const router = express.Router();
 
-// http://localhost:5000/api/v1/auth
+// http://localhost:5000/api/v1/auth/signup
 router.post('/signup', createUser);
-
+// http://localhost:5000/api/v1/auth/login
+router.post('/login', login);
 export default router;
